ae3f1754417f1ff59bbe7b0c35cd98daafcbdc4e
alejandro-sierra <alejandrosierraballesta@gmail.com>
2022-05-24 18:34:26 +0200

    [YQHDCH-12] feat: change docker-compose.yml

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import { CardRecipes } from './components/card-recipe/card-recipe.component';
 import { LoginPage } from './components/login-page/login-page/login-page.component';
 import { NavBar } from './components/nav-bar/nav-bar.component';
-import { useState, useAuth } from 'react';
+import { useState } from 'react';
+import { MainPage } from './components/main-page/main-page/main-page.component';
 import './App.css';
 
 
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <Routes>
-        <Route path='/' element={<CardRecipes/>}/>
+        <Route path='/' element={<MainPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
       </Routes>
     </div>
---
+++ src/components/main-page/main-form/main-form.component.jsx
@@ -0,0 +1,29 @@
+import { Button, Container, Form } from 'react-bootstrap';
+import './main-form.styles.css'
+
+export const MainForm = () => {
+
+    return (
+        <div className='main-form container-fluid mx-4'>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="number" min={1} max={7} placeholder="¿Cuántas recitas quieres?" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                
+                    <Form.Group className="mb-3">
+                        <Form.Label>Disabled select menu</Form.Label>
+                        <Form.Select>
+                            <option>Disabled select</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+        </div>
+    );
+}
